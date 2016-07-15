64879f2d42ad9334d0184b2b198540387e571a3e
Cyricx <contact@chrisbyram.com>
2016-07-14 22:17:31 -0500

    Added Faqs Route

---
+++ DexCMS.Faqs.Mvc/FaqsMvcRoutes.cs
@@ -0,0 +1,20 @@
+﻿using DexCMS.Core.Infrastructure.Models;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace DexCMS.Faqs
+{
+    public static class FaqsMvcRoutes
+    {
+        public static void CreateDefaultRoutes(RouteCollection routes, DexCMSConfiguration config)
+        {
+            routes.MapRoute(
+                name: "Faqs",
+                url: "faqs",
+                defaults: new { category = "none", controller = "FaqItems", action = "Index", urlSegment = "faqs" }
+            );
+
+        }
+    }
+
+}
