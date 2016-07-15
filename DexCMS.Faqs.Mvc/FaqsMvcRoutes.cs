428ba9848336531b1ea312f4e28b3f1aee3e873a
Cyricx <contact@chrisbyram.com>
2016-07-14 22:35:01 -0500

    Added routes for helpful/unhelpful marks

--- DexCMS.Faqs.Mvc/FaqsMvcRoutes.cs
+++ DexCMS.Faqs.Mvc/FaqsMvcRoutes.cs
@@ -13,6 +13,16 @@ namespace DexCMS.Faqs
                 url: "faqs",
                 defaults: new { category = "none", controller = "FaqItems", action = "Index", urlSegment = "faqs" }
             );
+            routes.MapRoute(
+                name: "FaqsHelpful",
+                url: "faqs/helpful/:id",
+                defaults: new { category = "none", controller = "FaqItems", action = "Helpful", urlSegment = "faqs" }
+            );
+            routes.MapRoute(
+                name: "FaqsUnhelpful",
+                url: "faqs/unhelpful/:id",
+                defaults: new { category = "none", controller = "FaqItems", action = "Unhelpful", urlSegment = "faqs" }
+            );
 
         }
     }
