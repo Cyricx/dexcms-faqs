42e2d536fb0c2e0a3524924ab21b07f0ef268d2d
Cyricx <contact@chrisbyram.com>
2016-11-26 11:11:49 -0600

    Added faqs mvc initializer

---
+++ DexCMS.Faqs.Mvc/Initializers/FaqsMvcInitializer.cs
@@ -0,0 +1,17 @@
+﻿using DexCMS.Base.Contexts;
+using DexCMS.Core.Infrastructure.Globals;
+
+namespace DexCMS.Faqs.Mvc.Initializers
+{
+    public class FaqsMvcInitializer : DexCMSInitializer<IDexCMSBaseContext>
+    {
+        public FaqsMvcInitializer(IDexCMSBaseContext context) : base(context)
+        {
+        }
+
+        public override void Run()
+        {
+            (new PageContentInitializer(Context)).Run();
+        }
+    }
+}
---
+++ DexCMS.Faqs.Mvc/Initializers/PageContentInitializer.cs
@@ -0,0 +1,40 @@
+﻿using DexCMS.Base.Contexts;
+using DexCMS.Base.Models;
+using DexCMS.Core.Infrastructure.Extensions;
+using DexCMS.Core.Infrastructure.Globals;
+using System;
+using System.Linq;
+
+namespace DexCMS.Faqs.Mvc.Initializers
+{
+    class PageContentInitializer : DexCMSInitializer<IDexCMSBaseContext>
+    {
+        public PageContentInitializer(IDexCMSBaseContext context) : base(context)
+        {
+        }
+
+        public override void Run()
+        {
+            DateTime Today = DateTime.Now;
+            int Public = Context.ContentAreas.Where(x => x.Name == "Public").Select(x => x.ContentAreaID).Single();
+            int SiteContent = Context.PageTypes.Where(x => x.Name == "Site Content").Select(x => x.PageTypeID).Single();
+
+            Context.PageContents.AddIfNotExists(x => x.PageTitle,
+                new PageContent
+                {
+                    Body = "",
+                    PageTitle = "Calendar",
+                    ChangeFrequency = 0,
+                    LastModified = Today,
+                    AddToSitemap = false,
+                    Heading = "Calendar",
+                    ContentAreaID = Public,
+                    ContentCategoryID = null,
+                    UrlSegment = "calendar",
+                    PageTypeID = SiteContent
+                }
+            );
+            Context.SaveChanges();
+        }
+    }
+}
